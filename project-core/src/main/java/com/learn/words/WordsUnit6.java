79241e0a615f5b40c1879d5634bf3ed13e3bf758
wzy <zhongyuan.wang@amh-group.com>
2021-11-02 14:22:51 +0800

    unit

--- project-core/src/main/java/com/learn/words/WordsUnit6.java
+++ project-core/src/main/java/com/learn/words/WordsUnit6.java
@@ -8,105 +8,92 @@ import com.learn.annotation.WordMean;
  * @date 2021/9/28
  */
 public class WordsUnit6 extends BaseWord{
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "place",desc = "n:地点,名词,地位,寓所,v:使处于(某种状态);放置;下(赌注)")
     private String word1;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "take place",desc = "举行; 进行; 产生;")
     private String word2;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "in place",desc = "在正确位置,准备妥当")
     private String word3;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "in place of",desc = "代替")
     private String word4;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "replace",desc = "v:代替,放回,归还")
     private String word5;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "replacement",desc = "n:替换,替代品,替代者")
     private String word6;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "displace",desc = "v:移动...的位置,迫使...离开家园,取代,置换")
     private String word7;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "believe",desc = "v:相信,认为")
     private String word8;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "belief",desc = "n:信仰,信念,相信")
     private String word9;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "unbelievable",desc = "adj:令人难以置信的")
     private String word10;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "credit",desc = "n:信用,信任,信贷,荣誉,学分,一笔钱,存款金额 v: 相信,信任")
     private String word11;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "mobile phone credit",desc = "手机话费")
     private String word12;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "the earned income tax credit",desc = "收入个人所得税")
     private String word13;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "faith",desc = "n:信任,相信")
     private String word14;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "faithful",desc = "adj:忠诚的,可信任的")
     private String word15;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "influence",desc = "n:影响,影响力,有影响力的人或事;v:影响,支配")
     private String word16;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "political influence",desc = "政治影响力")
     private String word17;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "influential",desc = "adj:有影响力的,有权势的")
     private String word18;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "data",desc = "n:数据,资料")
     private String word19;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "database",desc = "n:数据库")
     private String word20;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "observe",desc = "v:遵守,奉行,观察,注意到,看到,说,评论")
     private String word21;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "observation",desc = "n:观察,观测,监视,观察资料,言论")
     private String word22;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "observer",desc = "n:遵守者,观察者")
     private String word23;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "observable",desc = "adj:能察觉到的,看得见的")
     private String word24;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "serve",desc = "v:服务,接待,能满足...的需求,服役,任职")
     private String word25;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "service",desc = "n:服务,公共设施,维修,(政府的)部门;v:维修")
     private String word26;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "servant",desc = "n:仆人,(公司或机构的)雇员,职员")
     private String word27;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "deserve",desc = "v:应受,值得")
     private String word28;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "preserve",desc = "v:保护,维护,保存,保鲜 n:独占的地区(或领域/活动范围)")
     private String word29;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "reserve",desc = "n:保留,储备(量),储备金,矜持,拘谨 v:保留,储备,预定")
     private String word30;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "reserved",desc = "adj:矜持的,预定的")
     private String word31;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "conservation",desc = "n:(对环境,历史文物等的)保护,避免浪费(或损坏)")
     private String word32;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "conservative",desc = "adj:保守的,守旧的;n:保守者")
     private String word33;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "conservative forces",desc = "保守力量")
     private String word34;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "conservative views",desc = "守旧的观点")
     private String word35;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "abide",desc = "v:停留,保持,遵守,忍受,容忍")
     private String word36;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "glance",desc = "v:扫视;n:一瞥")
     private String word37;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "as first glance",desc = "乍一看")
     private String word38;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "a wistful glance",desc = "伤感的一瞥")
     private String word39;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "glimpse",desc = "n:一瞥;v:瞥见")
     private String word40;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "gaze",desc = "n/v:凝视")
     private String word41;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "gaze at",desc = "凝视,注视")
     private String word42;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "glare",desc = "n:怒视,瞪眼,炫耀,强光 v:怒目而视,发射强光")
     private String word43;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "glare at",desc = "怒视,瞪着")
     private String word44;
-    @WordMean(word = "",desc = "")
-    private String word45;
-    @WordMean(word = "",desc = "")
-    private String word46;
-    @WordMean(word = "",desc = "")
-    private String word47;
-    @WordMean(word = "",desc = "")
-    private String word48;
-    @WordMean(word = "",desc = "")
-    private String word49;
-    @WordMean(word = "",desc = "")
-    private String word50;
-
 }
