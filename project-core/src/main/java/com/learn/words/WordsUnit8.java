3101967d2f78b1d5633d77072cf9c9aee323b3aa
wzy <zhongyuan.wang@amh-group.com>
2021-11-10 14:26:57 +0800

    unit

---
+++ project-core/src/main/java/com/learn/words/WordsUnit8.java
@@ -0,0 +1,112 @@
+package com.learn.words;
+
+import com.learn.annotation.WordMean;
+
+/**
+ * @author wangzhongyuan
+ * @version 1.0
+ * @date 2021/9/28
+ */
+public class WordsUnit8 extends BaseWord{
+    @WordMean(word = "",desc = "")
+    private String word1;
+    @WordMean(word = "",desc = "")
+    private String word2;
+    @WordMean(word = "",desc = "")
+    private String word3;
+    @WordMean(word = "",desc = "")
+    private String word4;
+    @WordMean(word = "",desc = "")
+    private String word5;
+    @WordMean(word = "",desc = "")
+    private String word6;
+    @WordMean(word = "",desc = "")
+    private String word7;
+    @WordMean(word = "",desc = "")
+    private String word8;
+    @WordMean(word = "",desc = "")
+    private String word9;
+    @WordMean(word = "",desc = "")
+    private String word10;
+    @WordMean(word = "",desc = "")
+    private String word11;
+    @WordMean(word = "",desc = "")
+    private String word12;
+    @WordMean(word = "",desc = "")
+    private String word13;
+    @WordMean(word = "",desc = "")
+    private String word14;
+    @WordMean(word = "",desc = "")
+    private String word15;
+    @WordMean(word = "",desc = "")
+    private String word16;
+    @WordMean(word = "",desc = "")
+    private String word17;
+    @WordMean(word = "",desc = "")
+    private String word18;
+    @WordMean(word = "",desc = "")
+    private String word19;
+    @WordMean(word = "",desc = "")
+    private String word20;
+    @WordMean(word = "",desc = "")
+    private String word21;
+    @WordMean(word = "",desc = "")
+    private String word22;
+    @WordMean(word = "",desc = "")
+    private String word23;
+    @WordMean(word = "",desc = "")
+    private String word24;
+    @WordMean(word = "",desc = "")
+    private String word25;
+    @WordMean(word = "",desc = "")
+    private String word26;
+    @WordMean(word = "",desc = "")
+    private String word27;
+    @WordMean(word = "",desc = "")
+    private String word28;
+    @WordMean(word = "",desc = "")
+    private String word29;
+    @WordMean(word = "",desc = "")
+    private String word30;
+    @WordMean(word = "",desc = "")
+    private String word31;
+    @WordMean(word = "",desc = "")
+    private String word32;
+    @WordMean(word = "",desc = "")
+    private String word33;
+    @WordMean(word = "",desc = "")
+    private String word34;
+    @WordMean(word = "",desc = "")
+    private String word35;
+    @WordMean(word = "",desc = "")
+    private String word36;
+    @WordMean(word = "",desc = "")
+    private String word37;
+    @WordMean(word = "",desc = "")
+    private String word38;
+    @WordMean(word = "",desc = "")
+    private String word39;
+    @WordMean(word = "",desc = "")
+    private String word40;
+    @WordMean(word = "",desc = "")
+    private String word41;
+    @WordMean(word = "",desc = "")
+    private String word42;
+    @WordMean(word = "",desc = "")
+    private String word43;
+    @WordMean(word = "",desc = "")
+    private String word44;
+    @WordMean(word = "",desc = "")
+    private String word45;
+    @WordMean(word = "",desc = "")
+    private String word46;
+    @WordMean(word = "",desc = "")
+    private String word47;
+    @WordMean(word = "",desc = "")
+    private String word48;
+    @WordMean(word = "",desc = "")
+    private String word49;
+    @WordMean(word = "",desc = "")
+    private String word50;
+
+}
