2fc6335ab5d94f410cb8a2b45e1ab59a8af51651
wzy <zhongyuan.wang@amh-group.com>
2021-11-16 11:40:55 +0800

    i

--- project-core/src/main/java/com/learn/words/WordsUnit7.java
+++ project-core/src/main/java/com/learn/words/WordsUnit7.java
@@ -8,105 +8,106 @@ import com.learn.annotation.WordMean;
  * @date 2021/9/28
  */
 public class WordsUnit7 extends BaseWord{
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "create",desc = "v. 创造；创作；创建；引起，造成")
     private String word1;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "creation",desc = "n. 创造；创建；创作；(神的)创造物；天地万物")
     private String word2;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "creative",desc = "adj. 有创造力(或想象力)的；创造性的；启发想象力的")
     private String word3;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "creativity",desc = "n. 创造性；创造力")
     private String word4;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "recreate",desc = "v. 再创造；再创建；使再现")
     private String word5;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "recreation",desc = "n. 娱乐，消遣")
     private String word6;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "recreation facility",desc = "娱乐设施")
     private String word7;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "creature",desc = "n. 人；生物；动物；产物")
     private String word8;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "unchangeable creatures of habit",desc = "习惯不可改变的生物")
     private String word9;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "cooperative creatures",desc = "愿意合作的生物")
     private String word10;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "procreation",desc = "n. 生育,生殖")
     private String word11;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "culture",desc = "n. 文化，文明")
     private String word12;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "cultivate",desc = "v. 耕作，种 植；培养；结交(朋友) ")
     private String word13;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "cultivate political support",desc = "培养政治支持")
     private String word14;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "cultivation",desc = "n. 培养；教养；培植")
     private String word15;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "information",desc = "n. 通知；资料；信息")
     private String word16;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "inform",desc = "v. 通知；对…有影响；(on)告发")
+    private String word161;
+    @WordMean(word = "informed",desc = "adj. 了解情况的；见多识广的")
     private String word17;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "informal",desc = "adj. 非正式的")
     private String word18;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "informality",desc = "n. 非正式")
     private String word19;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "form",desc = "n. 形式；表格；形状;v. 形成")
     private String word20;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "in the form of",desc = "以…的形式")
     private String word21;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "formation",desc = "n. 形成；组成；编队，列队")
     private String word22;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "formal",desc = "adj. 正式的；形式上的；(指学校教育)正规的")
     private String word23;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "formal structures",desc = "形式结构")
     private String word24;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "formally",desc = "adv. 正式地；形式上地")
     private String word25;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "former",desc = "adj. 以前的；在前的;n. 前者")
     private String word26;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "formerly",desc = "adv. 从前，原来")
     private String word27;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "format",desc = "n. 设计；安排；样式;v. 安排版式；将…格式化")
     private String word28;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "formula",desc = "n. 公式，方程式；准则，方案")
     private String word29;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "formulation",desc = "n. （政策、计划等的）制定，构想；（想法的）阐述方式，表达方法；（药品或化妆品的）配方，配方产品")
     private String word30;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "formidable",desc = "adj. 可怕的；令人敬畏的；难以应付的")
     private String word31;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "formidable conductor",desc = "令人敬畏的指挥家")
     private String word32;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "perform",desc = "v. 履行；做；表演；表现(好或不好)")
     private String word33;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "performance",desc = "n. 履行；表演；性能，特性；表现；业绩；工作情况")
     private String word34;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "performative",desc = "adj. 表述行为的")
     private String word35;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "reform",desc = "n./v. 改革；改造，改良")
     private String word36;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "uniform",desc = "n. 制服;adj. 相同的，一律的；始终如一的")
     private String word37;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "uniformity",desc = "n. 一致，统一；一致性，统一性")
     private String word38;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "conform",desc = "v. 适应，顺从；遵守，服从；相符合，相一致")
     private String word39;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "conform to",desc = "符合…")
     private String word40;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "conformity",desc = "n. 一致，符合；顺从，遵照")
     private String word41;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "conformity to a standard pattern",desc = "对标准化模板的认同")
     private String word42;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "previous",desc = "adj. 以前的，先前的")
     private String word43;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "previously",desc = "adv. 先前，以前")
     private String word44;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "firm",desc = "adj. 坚固的；坚定的;n. 公司")
     private String word45;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "infirm",desc = "adj. 体弱的")
     private String word46;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "confirm",desc = "v. 确定;确认;(尤指提供证据来)证实；批准(职位、协议等)；使确信")
     private String word47;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "confirmation",desc = "n. 证实，证明")
     private String word48;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "affirm",desc = "v. 公开肯定；证实")
     private String word49;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "reaffirm",desc = "v. 重申，再次确定")
     private String word50;
-
 }
