76dfc884801d8290fdfcbfa009410e8cc620bb80
wzy <zhongyuan.wang@amh-group.com>
2021-11-17 18:48:58 +0800

    i

--- project-core/src/main/java/com/learn/words/WordsUnit8.java
+++ project-core/src/main/java/com/learn/words/WordsUnit8.java
@@ -8,105 +8,57 @@ import com.learn.annotation.WordMean;
  * @date 2021/9/28
  */
 public class WordsUnit8 extends BaseWord{
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "interest",desc = "n.兴趣,关注,吸引力,利害关系,利益,利息 v.引起...的关注,使...感兴趣")
     private String word1;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "personal interests",desc = "个人利益")
     private String word2;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "economic self-interest",desc = "经济上的利己主义")
     private String word3;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "public interest",desc = "公共利益")
     private String word4;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "interested",desc = "adj.感兴趣的,关心的,表新出兴趣的")
     private String word5;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "be interested in",desc = "对...感兴趣")
     private String word6;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "interesting",desc = "adj.有趣的,引起兴趣的")
     private String word7;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "product",desc = "n.产品,产物,(乘)积")
     private String word8;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "produce",desc = "v. 生产；制作；产生；拍摄(电 影、戏剧等);n. 产品；农产品")
     private String word9;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "production",desc = "n. 生产；产量；产生；上演")
     private String word10;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "productive",desc = "adj. 生产的；多产的；富有成效的")
     private String word11;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "productivity",desc = "n. 生产率；生产力")
     private String word12;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "reproduce",desc = "v. 生殖，繁殖；翻版；复制")
     private String word13;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "by-product",desc = "n. 副产品；附带产生的后果")
     private String word14;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "effect",desc = "n. 影响；结果；效果v. 产生，造成")
     private String word15;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "effective",desc = "adj. 有效的；生效的")
     private String word16;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "effectively",desc = "adv. 有效地")
     private String word17;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "effectiveness",desc = "n. 效力,有效性")
     private String word18;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "ineffective",desc = "adj. 不起作用的，无效果的")
     private String word19;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "ineffectiveness",desc = "n. 无效果,不起作用")
     private String word20;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "ineffectual",desc = "adj. 无效果的；(指人)无能的")
     private String word21;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "impact",desc = "n. 冲击；碰撞；影响，作用 v. 冲击；撞击；对…产生影响")
     private String word22;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "toll",desc = "n. 通行费；(事故等的)伤亡人数，(遭受的)损失；严重的不良影响；长途电话费;v. 向…征收捐税")
     private String word23;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "take a toll on sb/sth",desc = "(对某人或某事)产生恶果")
     private String word24;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "industry",desc = "n. 工业；产业；行业；企业；勤奋")
     private String word25;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "diligent",desc = "adj. 勤奋的，勤勉的")
     private String word26;
-    @WordMean(word = "",desc = "")
-    private String word27;
-    @WordMean(word = "",desc = "")
-    private String word28;
-    @WordMean(word = "",desc = "")
-    private String word29;
-    @WordMean(word = "",desc = "")
-    private String word30;
-    @WordMean(word = "",desc = "")
-    private String word31;
-    @WordMean(word = "",desc = "")
-    private String word32;
-    @WordMean(word = "",desc = "")
-    private String word33;
-    @WordMean(word = "",desc = "")
-    private String word34;
-    @WordMean(word = "",desc = "")
-    private String word35;
-    @WordMean(word = "",desc = "")
-    private String word36;
-    @WordMean(word = "",desc = "")
-    private String word37;
-    @WordMean(word = "",desc = "")
-    private String word38;
-    @WordMean(word = "",desc = "")
-    private String word39;
-    @WordMean(word = "",desc = "")
-    private String word40;
-    @WordMean(word = "",desc = "")
-    private String word41;
-    @WordMean(word = "",desc = "")
-    private String word42;
-    @WordMean(word = "",desc = "")
-    private String word43;
-    @WordMean(word = "",desc = "")
-    private String word44;
-    @WordMean(word = "",desc = "")
-    private String word45;
-    @WordMean(word = "",desc = "")
-    private String word46;
-    @WordMean(word = "",desc = "")
-    private String word47;
-    @WordMean(word = "",desc = "")
-    private String word48;
-    @WordMean(word = "",desc = "")
-    private String word49;
-    @WordMean(word = "",desc = "")
-    private String word50;
 
 }
