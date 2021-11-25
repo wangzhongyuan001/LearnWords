facf464582de126cc39e7d00271b73b193427340
wzy <zhongyuan.wang@amh-group.com>
2021-11-25 19:50:04 +0800

    i

--- project-core/src/main/java/com/learn/words/WordsUnit10.java
+++ project-core/src/main/java/com/learn/words/WordsUnit10.java
@@ -8,105 +8,105 @@ import com.learn.annotation.WordMean;
  * @date 2021/9/28
  */
 public class WordsUnit10 extends BaseWord{
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "back",desc = "n. 后面；背面；后背 adj. 后面的 adv. 在后；向后；回原处；以前 v. 后退；支持")
     private String word1;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "set back",desc = "延缓(某事的进展)；阻碍(某人完成某事)")
     private String word2;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "background",desc = "n. 背景；经历")
     private String word3;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "backyard",desc = "n. 后院；后花园")
     private String word4;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "backward",desc = "adj. 向后的；落后的；迟钝的 adv. 向后")
     private String word5;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "backdrop",desc = "n. (事件的)背景；(舞台后部的)背景幕布")
     private String word6;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "backload",desc = "v. (签订协议后)增加费用")
     private String word7;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "backfire",desc = "v. 产生事与愿违的不良后果；( 汽车引擎 ) 逆火，回火")
     private String word8;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "feedback",desc = "n. 反馈意见")
     private String word9;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "experience",desc = "n. 经验；经历 v. 经历")
     private String word10;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "experiment",desc = "n. 试验；实验 v. 进行实验")
     private String word11;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "expert",desc = "n. 专家；高手 adj. 熟练的，有经验的；专家的")
     private String word12;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "expertise",desc = "n. 专业知识(或技能等)，专长")
     private String word13;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "empirical",desc = "adj. 来自经验(或实验)的；经验主义的")
     private String word14;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "lead",desc = "v. 引导；在…中领先；引领；导致 n. 带领 n. 铅")
     private String word15;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "lead up to",desc = "导致；使话题渐渐转向")
     private String word16;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "plead",desc = "v. 恳求；辩护；提出…为理由")
     private String word17;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "value",desc = "n. 价值；价格；重要性；实用性；[pl.]价值观；(由字母或符号表示的)(数)值 v. 评价；估计；尊重，重视")
     private String word18;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "valuable",desc = "adj. 有价值的；值钱的")
     private String word19;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "overvalue",desc = "v. 对…估价过高；过分重视")
     private String word20;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "evaluate",desc = "v. 评估；估 价；求…的数值")
     private String word21;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "evaluation",desc = "n. 估价；评价；赋值")
     private String word22;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "estimate",desc = "v. 估计；估量 n. 估计，估算；估价")
     private String word23;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "underestimate",desc = "v. 低估；轻视 n. 低估；轻视")
     private String word24;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "assess",desc = "v. 评论，评价；对…评估")
     private String word25;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "assessment",desc = "n. 估价；评估")
     private String word26;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "price",desc = "n. 价格；代价 v. 给…定价")
     private String word27;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "rate",desc = "n. 速率；率；等级；价格，费用 v. 估价；评级，评价")
     private String word28;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "call",desc = "n. 喊，叫；打电话 v. 叫，喊；把…叫作；要求；拜访；(给…)打电话；召(至某处)")
     private String word29;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "recall",desc = "v. 回忆起，回想起；召回，收回 n. 召回令；回收令")
     private String word30;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "so-called",desc = "adj. 所谓的；号称的")
     private String word31;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "patent",desc = "adj. 有专利的；明显的 n. 专利，专利权 v. 申请专利；获得专利权")
     private String word32;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "invent",desc = "v. 发明，创造；虚构，编造")
     private String word33;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "invention",desc = "n. 发明，创造")
     private String word34;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "monopoly",desc = "n. 垄断；专卖；专利权；专利事业")
     private String word35;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "provide",desc = "v. 提供；供应")
     private String word36;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "provided",desc = "conj. 以…为条件；除非；假若")
     private String word37;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "provision",desc = "n. 供应(品)；准备；条款")
     private String word38;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "supply",desc = "v. 供给n. 供应(量)")
     private String word39;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "supplement",desc = "n. 增补(物)；(报纸的)增刊 v. 增补")
     private String word40;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "render",desc = "v. 使变得，使成为；给予，提供")
     private String word41;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "offer",desc = "v. 提供；出示；提议；(向神)奉上(祈祷、祭品等) n. 提议；提供；提供物")
     private String word42;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "fund",desc = "n. 专款，基金；资金，现款 v. 拨款；资助")
     private String word43;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "funding",desc = "n. 拨款")
     private String word44;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "refund",desc = "v. 退还，偿还，归还 n. 返还款，退款")
     private String word45;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "profound",desc = "adj. (影响)深远的，根深蒂固的；渊博的，造诣深的")
     private String word46;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "fundamental",desc = "adj. 基础的；基本的 n. 基本原则，基本原理")
     private String word47;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "fundamentally",desc = "adv. 基础地；根本地")
     private String word48;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "entrenched",desc = "adj. 根深蒂固的")
     private String word49;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "language",desc = "n. 语言")
     private String word50;
 
 }
