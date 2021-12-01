660f4f95fd77c83775d1610cf9fba33b69a616b9
wzy <zhongyuan.wang@amh-group.com>
2021-12-01 09:09:59 +0800

    i

--- project-core/src/main/java/com/learn/words/WordsUnit11.java
+++ project-core/src/main/java/com/learn/words/WordsUnit11.java
@@ -8,99 +8,102 @@ import com.learn.annotation.WordMean;
  * @date 2021/9/28
  */
 public class WordsUnit11 extends BaseWord{
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "linguistic",desc = "adj. 语言的；语言学的")
     private String word1;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "vocabulary",desc = "n. 词汇量；词汇表；(某一语言或学科的)词汇")
     private String word2;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "sentence",desc = "n. 句子；判决 v. 判决")
     private String word3;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "percent",desc = "n. 百分之…；百分比")
     private String word4;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "percentage",desc = "n. 百分比；百分率")
     private String word5;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "decade",desc = "n. 十年")
     private String word6;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "consider",desc = "v. 认为；把…看作；仔细考虑；体谅")
     private String word7;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "consideration",desc = "n. 要考虑的事；仔细考虑；体谅")
     private String word8;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "take sth into consideration",desc = "考虑到某事物；顾及某事物")
     private String word9;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "adequate consideration",desc = "充分的考虑")
     private String word10;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "considerable",desc = "adj. 相当大(或多)的；值得考虑的，重要的")
     private String word11;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "considerably",desc = "adv. 相当地，可观地")
     private String word12;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "contemplate",desc = "v. 思量， 对…作周密考虑；注视，凝视")
     private String word13;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "consumer",desc = "consumer")
     private String word14;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "consume",desc = "v. 消费；消耗； 吃，喝；使充满(某种强烈的感情) ")
     private String word15;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "consumerism",desc = "n. 用户至上主义；消费主义")
     private String word16;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "consumption",desc = "n. 消费；食用；消耗；消费量；消耗量")
     private String word17;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "assume",desc = "v. 假定，假设，臆断；承担；担任；呈现")
     private String word18;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "assumption",desc = "n. 假定，假设，臆断；(责任的)承担；(权利的)获得")
     private String word19;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "resume",desc = "v. (中断后)重新开始，继续，恢复")
     private String word20;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "resume one’s spirit",desc = "重新振作起来")
     private String word21;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "résumé",desc = "n. 摘要，概要；简历")
     private String word22;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "presumably",desc = "adv. 推测起来，大概")
     private String word23;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "improve",desc = "v. 改进，改善；提高")
     private String word24;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "improvement",desc = "n. 改进，改善；提高")
     private String word25;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "prove",desc = "v. 证明，证实；结果是，证明是")
     private String word26;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "proven",desc = "adj. 被证实的，经验证的")
     private String word27;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "verify",desc = "v. 核实，查证；证实，证明")
     private String word28;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "down",desc = "adv. 向下；到底地；(数量等)由大到小\n" +
+            "prep. 沿着；向下\n" +
+            "v. 击倒\n" +
+            "adj. 处于精神(或健康)低落状态的")
     private String word29;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "all the way down to",desc = "all the way down to")
     private String word30;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "be down to sb",desc = "是某人的责任；由某人负责")
     private String word31;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "downward",desc = "adj. 向下的 adv. (downwards) 向下地，往下地")
     private String word32;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "skill",desc = "n. 技能；技巧")
     private String word33;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "skilled",desc = "adj. 熟练的，有技能的；需要(专门)技术的")
     private String word34;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "skillful",desc = "adj. (in/at) 灵巧的；娴熟的")
     private String word35;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "concern",desc = "v. 涉及；使关心 n. 关心；关注；担忧；(利害)关系；重要的事情")
     private String word36;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "concerned",desc = "adj. 关注的；担忧的")
     private String word37;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "be concerned with/about",desc = "关切…，关注…")
     private String word38;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "as far as I am concerned",desc = "就我而言")
     private String word39;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "unconcerned",desc = "adj. 不关心的，不在意的")
     private String word40;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "be unconcerned with",desc = "对…不在意")
     private String word41;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "concerning",desc = "prep. 关于")
     private String word42;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "discern",desc = "v. 认出，觉察出；辨明，分清")
     private String word43;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "million",desc = "num. 百万；百万个")
     private String word44;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "millions of",desc = "数百万…；大量…")
     private String word45;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "mill",desc = "n. 磨粉机；磨坊；制造厂，工厂")
     private String word46;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "textile mill",desc = "纺织厂")
     private String word47;
     @WordMean(word = "",desc = "")
     private String word48;
