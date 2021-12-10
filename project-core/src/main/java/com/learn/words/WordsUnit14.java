3352811252020a9bf7a5a2ee1135429b447702a3
wzy <zhongyuan.wang@amh-group.com>
2021-12-10 10:47:58 +0800

    i

--- project-core/src/main/java/com/learn/words/WordsUnit14.java
+++ project-core/src/main/java/com/learn/words/WordsUnit14.java
@@ -8,105 +8,84 @@ import com.learn.annotation.WordMean;
  * @date 2021/9/28
  */
 public class WordsUnit14 extends BaseWord{
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "convey",desc = "v. 运送，输送；表达；传播")
     private String word1;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "shift",desc = "v. 转换；(使)转移(地点或位置)；(意见、情况、政策)发生变动")
     private String word2;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "shift",desc = "n. 转换，转变；轮班，换班")
     private String word3;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "generation",desc = "n. 产生，发生；一代人")
     private String word4;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "generate",desc = "v. 产生，发 生；引起；产生(能量)，发(电) ")
     private String word5;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "generative",desc = "adj. 能生产的；促使…生成的")
     private String word6;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "set",desc = "n. (一)套，系列 v. 树立(榜样等)；校正；(日、月等)落； 放，置；开始做；设置 adj. 固定不变的，确定的，安排好的")
     private String word7;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "set aside",desc = "挑出，拨出，留出；不理会")
     private String word8;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "set off",desc = "出发，动身；引起")
     private String word9;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "set up",desc = "建立，树立；开办")
     private String word10;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "setting",desc = "n. (天体等)沉落；(机器的)底座；环境，背景，地点")
     private String word11;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "settle",desc = "v. 安顿；使定居；解决； (使)平静，(使)安定")
     private String word12;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "settlement",desc = "n. 安顿；定居；定居点；殖民地；和解")
     private String word13;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "setback",desc = "n. 挫折；倒退；失败")
     private String word14;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "set back",desc = "阻碍；使受挫折")
     private String word15;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "encounter setbacks",desc = "遭遇挫折")
     private String word16;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "tend",desc = "v. 趋向，往往会，倾向；照 料，护理")
     private String word17;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "tendency",desc = "n. 倾向，趋势")
     private String word18;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "a tendency to do sth",desc = "做某事的倾向；做某事的趋势")
     private String word19;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "intend",desc = "v. 想要，打算，计划")
     private String word20;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "intended",desc = "adj. 有意的，故意的")
     private String word21;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "intended meaning",desc = "意图")
     private String word22;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "be intended to do sth",desc = "打算做某事")
     private String word23;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "intention",desc = "n. 意图，意向，目的；意思，含义")
     private String word24;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "intentional",desc = "adj. 故意的，蓄意的")
     private String word25;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "intentional racial discrimination",desc = "故意种族歧视")
     private String word26;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "intentionally",desc = "adv. 有意地，故意地")
     private String word27;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "unintentional",desc = "adj. 无心的，无意的")
     private String word28;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "unintentionally",desc = "adv. 无意之中，非故意地")
     private String word29;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "pretend",desc = "v. 假装；(演戏等中)装扮")
     private String word30;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "deal",desc = "n. 交易，协议；数量；程度 v. 处理")
     private String word31;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "a great deal of",desc = "大量(的)，许多(的)")
     private String word32;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "a good deal",desc = "很多的，大量的")
     private String word33;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "big deal",desc = "重要的事情(或情况)")
     private String word34;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "dealing",desc = "n. 行为；交易；交往")
     private String word35;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "dealer",desc = "n. 商人；证券经纪人")
     private String word36;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "idea",desc = "n. 思想；概念；主意")
     private String word37;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "ideal",desc = "adj. 理想的；空想的；理想主义的；唯心的 n. 理想")
     private String word38;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "ideology",desc = "n. 意识形态， (政治或社会的)思想意识，思想体系")
     private String word39;
-    @WordMean(word = "",desc = "")
+    @WordMean(word = "transition",desc = "n. 转变，过渡")
     private String word40;
-    @WordMean(word = "",desc = "")
-    private String word41;
-    @WordMean(word = "",desc = "")
-    private String word42;
-    @WordMean(word = "",desc = "")
-    private String word43;
-    @WordMean(word = "",desc = "")
-    private String word44;
-    @WordMean(word = "",desc = "")
-    private String word45;
-    @WordMean(word = "",desc = "")
-    private String word46;
-    @WordMean(word = "",desc = "")
-    private String word47;
-    @WordMean(word = "",desc = "")
-    private String word48;
-    @WordMean(word = "",desc = "")
-    private String word49;
-    @WordMean(word = "",desc = "")
-    private String word50;
-
 }
