27f7ad6f59913048ae687816fcb4424f09ca4d25
wzy <zhongyuan.wang@amh-group.com>
2021-12-12 08:16:56 +0800

    i

---
+++ project-core/src/main/java/com/learn/words/AgainWords.java
@@ -0,0 +1,412 @@
+package com.learn.words;
+
+import com.learn.annotation.WordMean;
+
+/**
+ * @author wangzhongyuan
+ * @version 1.0
+ * @date 2021/9/28
+ */
+public class AgainWords extends BaseWord{
+    @WordMean(word = "",desc = "")
+    private String word1;
+    @WordMean(word = "",desc = "")
+    private String word2;
+    @WordMean(word = "",desc = "")
+    private String word3;
+    @WordMean(word = "",desc = "")
+    private String word4;
+    @WordMean(word = "",desc = "")
+    private String word5;
+    @WordMean(word = "",desc = "")
+    private String word6;
+    @WordMean(word = "",desc = "")
+    private String word7;
+    @WordMean(word = "",desc = "")
+    private String word8;
+    @WordMean(word = "",desc = "")
+    private String word9;
+    @WordMean(word = "",desc = "")
+    private String word10;
+    @WordMean(word = "",desc = "")
+    private String word11;
+    @WordMean(word = "",desc = "")
+    private String word12;
+    @WordMean(word = "",desc = "")
+    private String word13;
+    @WordMean(word = "",desc = "")
+    private String word14;
+    @WordMean(word = "",desc = "")
+    private String word15;
+    @WordMean(word = "",desc = "")
+    private String word16;
+    @WordMean(word = "",desc = "")
+    private String word17;
+    @WordMean(word = "",desc = "")
+    private String word18;
+    @WordMean(word = "",desc = "")
+    private String word19;
+    @WordMean(word = "",desc = "")
+    private String word20;
+    @WordMean(word = "",desc = "")
+    private String word21;
+    @WordMean(word = "",desc = "")
+    private String word22;
+    @WordMean(word = "",desc = "")
+    private String word23;
+    @WordMean(word = "",desc = "")
+    private String word24;
+    @WordMean(word = "",desc = "")
+    private String word25;
+    @WordMean(word = "",desc = "")
+    private String word26;
+    @WordMean(word = "",desc = "")
+    private String word27;
+    @WordMean(word = "",desc = "")
+    private String word28;
+    @WordMean(word = "",desc = "")
+    private String word29;
+    @WordMean(word = "",desc = "")
+    private String word30;
+    @WordMean(word = "",desc = "")
+    private String word31;
+    @WordMean(word = "",desc = "")
+    private String word32;
+    @WordMean(word = "",desc = "")
+    private String word33;
+    @WordMean(word = "",desc = "")
+    private String word34;
+    @WordMean(word = "",desc = "")
+    private String word35;
+    @WordMean(word = "",desc = "")
+    private String word36;
+    @WordMean(word = "",desc = "")
+    private String word37;
+    @WordMean(word = "",desc = "")
+    private String word38;
+    @WordMean(word = "",desc = "")
+    private String word39;
+    @WordMean(word = "",desc = "")
+    private String word40;
+    @WordMean(word = "",desc = "")
+    private String word41;
+    @WordMean(word = "",desc = "")
+    private String word42;
+    @WordMean(word = "",desc = "")
+    private String word43;
+    @WordMean(word = "",desc = "")
+    private String word44;
+    @WordMean(word = "",desc = "")
+    private String word45;
+    @WordMean(word = "",desc = "")
+    private String word46;
+    @WordMean(word = "",desc = "")
+    private String word47;
+    @WordMean(word = "",desc = "")
+    private String word48;
+    @WordMean(word = "",desc = "")
+    private String word49;
+    @WordMean(word = "",desc = "")
+    private String word50;
+    @WordMean(word = "",desc = "")
+    private String word51;
+    @WordMean(word = "",desc = "")
+    private String word52;
+    @WordMean(word = "",desc = "")
+    private String word53;
+    @WordMean(word = "",desc = "")
+    private String word54;
+    @WordMean(word = "",desc = "")
+    private String word55;
+    @WordMean(word = "",desc = "")
+    private String word56;
+    @WordMean(word = "",desc = "")
+    private String word57;
+    @WordMean(word = "",desc = "")
+    private String word58;
+    @WordMean(word = "",desc = "")
+    private String word59;
+    @WordMean(word = "",desc = "")
+    private String word60;
+    @WordMean(word = "",desc = "")
+    private String word61;
+    @WordMean(word = "",desc = "")
+    private String word62;
+    @WordMean(word = "",desc = "")
+    private String word63;
+    @WordMean(word = "",desc = "")
+    private String word64;
+    @WordMean(word = "",desc = "")
+    private String word65;
+    @WordMean(word = "",desc = "")
+    private String word66;
+    @WordMean(word = "",desc = "")
+    private String word67;
+    @WordMean(word = "",desc = "")
+    private String word68;
+    @WordMean(word = "",desc = "")
+    private String word69;
+    @WordMean(word = "",desc = "")
+    private String word70;
+    @WordMean(word = "",desc = "")
+    private String word71;
+    @WordMean(word = "",desc = "")
+    private String word72;
+    @WordMean(word = "",desc = "")
+    private String word73;
+    @WordMean(word = "",desc = "")
+    private String word74;
+    @WordMean(word = "",desc = "")
+    private String word75;
+    @WordMean(word = "",desc = "")
+    private String word76;
+    @WordMean(word = "",desc = "")
+    private String word77;
+    @WordMean(word = "",desc = "")
+    private String word78;
+    @WordMean(word = "",desc = "")
+    private String word79;
+    @WordMean(word = "",desc = "")
+    private String word80;
+    @WordMean(word = "",desc = "")
+    private String word81;
+    @WordMean(word = "",desc = "")
+    private String word82;
+    @WordMean(word = "",desc = "")
+    private String word83;
+    @WordMean(word = "",desc = "")
+    private String word84;
+    @WordMean(word = "",desc = "")
+    private String word85;
+    @WordMean(word = "",desc = "")
+    private String word86;
+    @WordMean(word = "",desc = "")
+    private String word87;
+    @WordMean(word = "",desc = "")
+    private String word88;
+    @WordMean(word = "",desc = "")
+    private String word89;
+    @WordMean(word = "",desc = "")
+    private String word90;
+    @WordMean(word = "",desc = "")
+    private String word91;
+    @WordMean(word = "",desc = "")
+    private String word92;
+    @WordMean(word = "",desc = "")
+    private String word93;
+    @WordMean(word = "",desc = "")
+    private String word94;
+    @WordMean(word = "",desc = "")
+    private String word95;
+    @WordMean(word = "",desc = "")
+    private String word96;
+    @WordMean(word = "",desc = "")
+    private String word97;
+    @WordMean(word = "",desc = "")
+    private String word98;
+    @WordMean(word = "",desc = "")
+    private String word99;
+    @WordMean(word = "",desc = "")
+    private String word100;
+    private String word101;
+    private String word102;
+    private String word103;
+    private String word104;
+    private String word105;
+    private String word106;
+    private String word107;
+    private String word108;
+    private String word109;
+    private String word110;
+    private String word111;
+    private String word112;
+    private String word113;
+    private String word114;
+    private String word115;
+    private String word116;
+    private String word117;
+    private String word118;
+    private String word119;
+    private String word120;
+    private String word121;
+    private String word122;
+    private String word123;
+    private String word124;
+    private String word125;
+    private String word126;
+    private String word127;
+    private String word128;
+    private String word129;
+    private String word130;
+    private String word131;
+    private String word132;
+    private String word133;
+    private String word134;
+    private String word135;
+    private String word136;
+    private String word137;
+    private String word138;
+    private String word139;
+    private String word140;
+    private String word141;
+    private String word142;
+    private String word143;
+    private String word144;
+    private String word145;
+    private String word146;
+    private String word147;
+    private String word148;
+    private String word149;
+    private String word150;
+    private String word151;
+    private String word152;
+    private String word153;
+    private String word154;
+    private String word155;
+    private String word156;
+    private String word157;
+    private String word158;
+    private String word159;
+    private String word160;
+    private String word161;
+    private String word162;
+    private String word163;
+    private String word164;
+    private String word165;
+    private String word166;
+    private String word167;
+    private String word168;
+    private String word169;
+    private String word170;
+    private String word171;
+    private String word172;
+    private String word173;
+    private String word174;
+    private String word175;
+    private String word176;
+    private String word177;
+    private String word178;
+    private String word179;
+    private String word180;
+    private String word181;
+    private String word182;
+    private String word183;
+    private String word184;
+    private String word185;
+    private String word186;
+    private String word187;
+    private String word188;
+    private String word189;
+    private String word190;
+    private String word191;
+    private String word192;
+    private String word193;
+    private String word194;
+    private String word195;
+    private String word196;
+    private String word197;
+    private String word198;
+    private String word199;
+    private String word200;
+    private String word201;
+    private String word202;
+    private String word203;
+    private String word204;
+    private String word205;
+    private String word206;
+    private String word207;
+    private String word208;
+    private String word209;
+    private String word210;
+    private String word211;
+    private String word212;
+    private String word213;
+    private String word214;
+    private String word215;
+    private String word216;
+    private String word217;
+    private String word218;
+    private String word219;
+    private String word220;
+    private String word221;
+    private String word222;
+    private String word223;
+    private String word224;
+    private String word225;
+    private String word226;
+    private String word227;
+    private String word228;
+    private String word229;
+    private String word230;
+    private String word231;
+    private String word232;
+    private String word233;
+    private String word234;
+    private String word235;
+    private String word236;
+    private String word237;
+    private String word238;
+    private String word239;
+    private String word240;
+    private String word241;
+    private String word242;
+    private String word243;
+    private String word244;
+    private String word245;
+    private String word246;
+    private String word247;
+    private String word248;
+    private String word249;
+    private String word250;
+    private String word251;
+    private String word252;
+    private String word253;
+    private String word254;
+    private String word255;
+    private String word256;
+    private String word257;
+    private String word258;
+    private String word259;
+    private String word260;
+    private String word261;
+    private String word262;
+    private String word263;
+    private String word264;
+    private String word265;
+    private String word266;
+    private String word267;
+    private String word268;
+    private String word269;
+    private String word270;
+    private String word271;
+    private String word272;
+    private String word273;
+    private String word274;
+    private String word275;
+    private String word276;
+    private String word277;
+    private String word278;
+    private String word279;
+    private String word280;
+    private String word281;
+    private String word282;
+    private String word283;
+    private String word284;
+    private String word285;
+    private String word286;
+    private String word287;
+    private String word288;
+    private String word289;
+    private String word290;
+    private String word291;
+    private String word292;
+    private String word293;
+    private String word294;
+    private String word295;
+    private String word296;
+    private String word297;
+    private String word298;
+    private String word299;
+    private String word300;
+
+}
